684eddde6cdd0a463a422650e8415ac6d8159691
VGLoic <vincent.genod.loic@gmail.com>
2026-03-01 23:45:13 +0100

    restore e2e test, except for deployment

--- apps/hardhat-v3_etherscan-verification/e2e-test/push-pull-deploy.e2e.test.ts
+++ apps/hardhat-v3_etherscan-verification/e2e-test/push-pull-deploy.e2e.test.ts
@@ -15,25 +15,26 @@ describe("[Hardhat v3 - Etherscan Verification] Push artifact, pull artifact, de
       `npx hardhat --config ./hardhat.config.e2e.ts ethoko push --tag ${TAG_NAME} --debug`,
     ));
 
-  // test("it pulls the tag", () =>
-  //   asyncExec("npx hardhat --config ./hardhat.config.e2e.ts ethoko pull"));
+  test("it pulls the tag", () =>
+    asyncExec("npx hardhat --config ./hardhat.config.e2e.ts ethoko pull"));
 
-  // test("it generates the typings", () =>
-  //   asyncExec("npx hardhat --config ./hardhat.config.e2e.ts ethoko typings"));
+  test("it generates the typings", () =>
+    asyncExec("npx hardhat --config ./hardhat.config.e2e.ts ethoko typings"));
 
-  // test("it checks types", () => asyncExec("pnpm tsc --noEmit"));
+  test("it checks types", () => asyncExec("pnpm tsc --noEmit"));
 
+  // REMIND ME
   // test("it deploys", () =>
   //   asyncExec(
   //     "npx hardhat ignition deploy ./ignition/modules/counter-2026-02-02.ts --config ./hardhat.config.e2e.ts ",
   //   ));
 
-  // test("it restores the original artifacts", async () => {
-  //   await asyncExec(
-  //     `npx hardhat --config ./hardhat.config.e2e.ts ethoko restore --tag ${TAG_NAME} --output ./${E2E_FOLDER_PATH}/restored-artifacts-${TAG_NAME}`,
-  //   );
-  //   await asyncExec(
-  //     `ls -la ./${E2E_FOLDER_PATH}/restored-artifacts-${TAG_NAME}`,
-  //   );
-  // });
+  test("it restores the original artifacts", async () => {
+    await asyncExec(
+      `npx hardhat --config ./hardhat.config.e2e.ts ethoko restore --tag ${TAG_NAME} --output ./${E2E_FOLDER_PATH}/restored-artifacts-${TAG_NAME}`,
+    );
+    await asyncExec(
+      `ls -la ./${E2E_FOLDER_PATH}/restored-artifacts-${TAG_NAME}`,
+    );
+  });
 });
