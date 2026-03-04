ffd5ce4f0a321a8ae02273eccaad05645e4904de
VGLoic <vincent.genod.loic@gmail.com>
2026-03-04 17:52:33 +0100

    add ownable from oz to hardhat-v3 etherscan integration example

--- apps/hardhat-v3_etherscan-verification/contracts/Oracle.sol
+++ apps/hardhat-v3_etherscan-verification/contracts/Oracle.sol
@@ -1,12 +1,16 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
 
-contract Oracle {
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Oracle is Ownable {
     uint public by;
+
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
     event Updated(uint by);
 
-    function set(uint _by) public {
+    function set(uint _by) public onlyOwner {
         by = _by;
         emit Updated(_by);
     }
--- apps/hardhat-v3_etherscan-verification/e2e-test/push-pull-deploy.e2e.test.ts
+++ apps/hardhat-v3_etherscan-verification/e2e-test/push-pull-deploy.e2e.test.ts
@@ -7,7 +7,7 @@ const TAG_NAME = "2026-02-02";
 describe("[Hardhat v3 - Etherscan Verification] Push artifact, pull artifact, deploy", async () => {
   test("it compiles", () =>
     asyncExec(
-      "npx hardhat build --build-profile production --config ./hardhat.config.e2e.ts",
+      "npx hardhat build --build-profile production --config ./hardhat.config.e2e.ts --force",
     ));
 
   test("it pushes the tag", () =>
