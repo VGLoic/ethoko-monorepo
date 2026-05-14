82bb9e3260e1c0462a243fc1555403532efbb798
VGLoic <vincent.genod.loic@gmail.com>
2026-05-14 17:38:41 +0200

    introduce request in model, include new types construction

--- apps/central/src/users/auth_router.rs
+++ apps/central/src/users/auth_router.rs
@@ -1,7 +1,11 @@
+use crate::{
+    newtypes::{email::EmailError, handle::HandleError},
+    router::ApiError,
+};
 use axum::{Json, Router, http::StatusCode, response::IntoResponse, routing::post};
 use serde::{Deserialize, Serialize};
 
-use crate::newtypes::{email::Email, handle::Handle};
+use super::models::{EmailSignupRequest, EmailSignupRequestError};
 
 pub fn auth_router() -> Router {
     Router::new().route("/signup/email", post(handle_signup_email))
@@ -14,15 +18,34 @@ pub struct SignupEmailBody {
     pub password: String,
 }
 
-async fn handle_signup_email(Json(body): Json<SignupEmailBody>) -> impl IntoResponse {
-    let email = match Email::new(&body.email) {
-        Ok(email) => email,
-        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid email format").into_response(),
-    };
-    let handle = match Handle::new(&body.handle) {
-        Ok(handle) => handle,
-        Err(_) => return (StatusCode::BAD_REQUEST, "Invalid handle format").into_response(),
+async fn handle_signup_email(
+    Json(body): Json<SignupEmailBody>,
+) -> Result<(StatusCode, String), ApiError> {
+    let request = match EmailSignupRequest::new(body.email, body.handle, body.password) {
+        Ok(req) => req,
+        Err(e) => {
+            match e {
+                EmailSignupRequestError::InvalidEmail(err) => {
+                    let error_message = match err {
+                        EmailError::Empty => "\"email\": empty value not allowed".to_string(),
+                        EmailError::InvalidFormat => "\"email\": invalid format".to_string(),
+                    };
+                    return Err(ApiError::BadRequest(error_message));
+                }
+                EmailSignupRequestError::InvalidHandle(err) => {
+                    let error_message = match err {
+                        HandleError::Empty => "\"handle\": empty value not allowed".to_string(),
+                        HandleError::InvalidFormat => "\"handle\": invalid format, expected only alphanumeric characters and hyphens, length between 4 and 31".to_string(),
+                        HandleError::InvalidSpecificHandle => "\"handle\": value is not allowed".to_string(),
+                    };
+                    return Err(ApiError::BadRequest(error_message));
+                }
+                EmailSignupRequestError::Unknown(err) => {
+                    return Err(ApiError::InternalServerError(err));
+                }
+            };
+        }
     };
 
-    (StatusCode::CREATED, "Signup email endpoint").into_response()
+    Ok((StatusCode::CREATED, "Signup email endpoint".into()))
 }
---
+++ apps/central/src/users/models.rs
@@ -0,0 +1,86 @@
+use thiserror::Error;
+
+use crate::newtypes::{
+    email::{Email, EmailError},
+    handle::{Handle, HandleError},
+};
+
+pub struct EmailSignupRequest {
+    pub email: Email,
+    pub handle: Handle,
+    pub password_hash: String,
+}
+
+#[derive(Debug, Error)]
+pub enum EmailSignupRequestError {
+    #[error("Invalid email format")]
+    InvalidEmail(EmailError),
+    #[error("Invalid handle format")]
+    InvalidHandle(HandleError),
+    #[error(transparent)]
+    Unknown(#[from] anyhow::Error),
+}
+
+impl EmailSignupRequest {
+    /// Creates a new `EmailSignupRequest` after validating the formats and hashing the password.
+    ///
+    /// # Errors
+    /// `EmailSgnupRequestError::InvalidEmail` if the email format is invalid.
+    /// `EmailSgnupRequestError::InvalidHandle` if the handle format is invalid
+    /// `EmailSgnupRequestError::Unknown` for any other errors that may occur during the process.
+    pub fn new(
+        email: String,
+        handle: String,
+        password: String,
+    ) -> Result<Self, EmailSignupRequestError> {
+        let email = Email::new(&email).map_err(|e| EmailSignupRequestError::InvalidEmail(e))?;
+        let handle = Handle::new(&handle).map_err(|e| EmailSignupRequestError::InvalidHandle(e))?;
+        let password_hash = password; // REMIND ME
+        Ok(Self {
+            email,
+            handle,
+            password_hash,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use fake::{Fake, Faker};
+
+    use super::*;
+
+    #[test]
+    fn test_valid_email_signup_request() {
+        let email = Faker.fake::<Email>();
+        let handle = Faker.fake::<Handle>();
+        let password = Faker.fake::<String>();
+
+        let result = EmailSignupRequest::new(email.to_string(), handle.to_string(), password);
+        assert!(result.is_ok());
+    }
+
+    #[test]
+    fn test_invalid_email_signup_request() {
+        let invalid_email = "invalid-email".to_string();
+        let handle = Faker.fake::<Handle>();
+        let password = Faker.fake::<String>();
+        let result = EmailSignupRequest::new(invalid_email, handle.to_string(), password);
+        assert!(matches!(
+            result,
+            Err(EmailSignupRequestError::InvalidEmail(_))
+        ));
+    }
+
+    #[test]
+    fn test_invalid_handle_signup_request() {
+        let email = Faker.fake::<Email>();
+        let invalid_handle = "invalid-handle".to_string();
+        let password = Faker.fake::<String>();
+        let result = EmailSignupRequest::new(email.to_string(), invalid_handle, password);
+        assert!(matches!(
+            result,
+            Err(EmailSignupRequestError::InvalidHandle(_))
+        ));
+    }
+}
